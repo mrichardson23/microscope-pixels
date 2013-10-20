e1a09c5c8e70322b3f65db30443af51d2646c926
Matt Richardson <mrichardson23@gmail.com>
2013-10-20 15:56:14 -0400

    serial mode changer

--- microscope_pixels.ino
+++ microscope_pixels.ino
@@ -2,6 +2,7 @@
 
 #define PIN 6
 #define PIXELS 17
+#define STEPS_PER_MODE 4
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
@@ -18,26 +19,49 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELS, PIN, NEO_GRB + NEO_KHZ800);
 // tab.
 #include "colorPatterns.h"
 
+int mode = 0;
+int step = 0;
 
 void setup() {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+  Serial.begin(9600);
 }
 
 void loop() {
-  
-  for (int i = 0; i < modes; i++) {
-    for (int j = 0; j < PIXELS; j+=2)
-    {
-      setColorMode(i, j);
-      delay(1000);
+  while (Serial.available()) {
+    // if we receive a serial byte...
+    if (step < PIXELS) {
+      // if we have more steps to go before filling the strip,
+      // add one "step" to the current mode.
+      step += PIXELS / STEPS_PER_MODE;
     }
+    else {
+      // if the strip is full, we go back to step 0
+      step = 0;
+      if (mode < modes-1) {
+        // if we have more modes, we go to the next.
+        mode++;
+      }
+      else {
+        // if we're at the last mode, go to the first.
+        mode = 0;
+      }
+    }
+    // set the mode and step:
+    setColorMode(mode, step);
+
+    Serial.print(mode);
+    Serial.print(",");
+    Serial.println(step);
+    
+    Serial.read(); // remove the byte from the buffer
   }
-  
 }
 
-
 void setColorMode(int mode) {
+  // This function reads all the pixels in the
+  // mode and sets the pixels accordingly.
   for (int i = 0; i < PIXELS -1; i++) {
     strip.setPixelColor(i, colorModes[mode][i]);
   }
@@ -45,6 +69,8 @@ void setColorMode(int mode) {
 }
 
 void setColorMode(int mode, int pixels) {
+  // This function only sets the first specified
+  // number of pixels. The remainder will be dark.
   clearPixels();
   for (int i = 0; i < pixels -1; i++) {
     strip.setPixelColor(i, colorModes[mode][i]);
@@ -53,8 +79,9 @@ void setColorMode(int mode, int pixels) {
 }
 
 void clearPixels() {
-  for (int i = 0; i < PIXELS -1; i++) {
+  for (int i = 0; i < PIXELS; i++) {
     strip.setPixelColor(i,0,0,0);
   }
 }
+
 
